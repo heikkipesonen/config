ad7db97126d09e7eae4041a026a5a14553478e29
heikki pesonen <heikki.pesonen@sok.fi>
2025-12-04 15:30:55 +0200

    fart

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,14 @@
+return {
+  "folke/snacks.nvim",
+  ---@type snacks.Config
+  opts = {
+    explorer = {},
+    picker = {
+      sources = {
+        files = { hidden = true },
+        grep = { hidden = true },
+        explorer = { hidden = true },
+      },
+    },
+  },
+}
