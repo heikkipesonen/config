91d3b245a1385061a0b64de61f3f1512f2d6b695
heikkipesonen <pesonen.heikki@gmail.com>
2026-01-05 09:13:35 +0200

    add keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("n", "å", "[", { noremap = true })
+vim.keymap.set("n", "¨", "]", { noremap = true })
+vim.keymap.set("n", "ä", "{", { noremap = true })
+vim.keymap.set("n", "'", "}", { noremap = true })
